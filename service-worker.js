461dfa622c48d73641590391843963cf4f5b0dbf
thuyaaungzaw2-lang <sub4u9999@gmail.com>
2025-12-05 01:38:55 +0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,98 @@
+[file name]: service-worker.js
+[file content begin]
+const CACHE_NAME = 'videodl-v1.0';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  // Add other assets if you have (CSS, JS files, etc.)
+];
+
+// Install event
+self.addEventListener('install', event => {
+  console.log('[Service Worker] Installing...');
+  
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('[Service Worker] Caching app shell');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+      .then(() => {
+        console.log('[Service Worker] Install completed');
+        return self.skipWaiting();
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  console.log('[Service Worker] Activating...');
+  
+  // Remove old caches
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('[Service Worker] Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => {
+      console.log('[Service Worker] Activation completed');
+      return self.clients.claim();
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  // Don't cache API requests
+  if (event.request.url.includes('/formats') || event.request.url.includes('/download')) {
+    return;
+  }
+  
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version if found
+        if (response) {
+          return response;
+        }
+        
+        // Otherwise fetch from network
+        return fetch(event.request)
+          .then(response => {
+            // Don't cache if not successful
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            
+            // Clone the response
+            const responseToCache = response.clone();
+            
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+              
+            return response;
+          });
+      })
+      .catch(() => {
+        // If both cache and network fail, show offline page
+        return caches.match('/')
+          .then(response => response || new Response('Offline'));
+      })
+  );
+});
+
+// Handle messages from main thread
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+[file content end]
